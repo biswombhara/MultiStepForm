967a9c31591f0f6e60ba606418f781b0be5ef163
Biswombhara <biswombhara.patra@gmail.com>
2024-05-23 13:26:39 +0530

    update 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Form from "./Form";
+import {useEffect, useRef } from "react";
 import loading from './assets/loading.gif'
 function App() {
-  window.addEventListener("load",()=>{
-    document.getElementById("loader").style.display="none";
-  })
+  let ref =useRef();
+  useEffect(()=>{
+    let el = ref.current;
+      setTimeout(()=>{
+        el.style.display = "none"
+      },2000);    
+  },[])
   return (
     <div>
-      <div id="loader" className="h-screen flex items-center justify-center h-screen w-screen fixed z-10 bg-white">
+          <div ref={ref} className=" h-screen flex items-center justify-center h-screen w-screen fixed z-10 bg-white">
         <img className=" scale-[0.35]" src={loading} alt="load"></img>
-      </div>
+        </div>
       <Form />
     </div>
   );
