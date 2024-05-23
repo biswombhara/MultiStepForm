a6fd706be986bff30b27f6395bdb8791ce7cd225
Biswombhara <biswombhara.patra@gmail.com>
2024-05-23 12:31:58 +0530

    update 6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Form from "./Form";
 import loading from './assets/loading.gif'
 function App() {
+  window.addEventListener("load",()=>{
+    document.getElementById("loader").style.display="none";
+  })
   return (
     <div>
       <div id="loader" className="h-screen flex items-center justify-center h-screen w-screen fixed z-10 bg-white">
